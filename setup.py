aab5d81094de1ed5a2480cb98912c5051816c35f
Jesse Milam <jessemilam@gmail.com>
2023-04-06 16:17:08 -0400

    Removed build files from source control
    Removed build files from source control

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 __currdir__ = os.getcwd()
 __readme__ = os.path.join(__currdir__, 'README.md')
 
+<<<<<<< HEAD
 version = '1.0.10'
 
 if __name__ == '__main__':
@@ -27,3 +28,25 @@ if __name__ == '__main__':
             'openpyxl'
         ]
     )
+=======
+setup(
+    name='plantpredict',
+    version='1.0.9',
+    description='Python SDK for PlantPredict (https://ui.plantpredict.terabase.energy).',
+    url='https://github.com/plantpredict/python-sdk',
+    author='Stephen Kaplan, Performance & Prediction Engineer at First Solar, Inc.',
+    author_email='stephen.kaplan@firstsolar.com',
+    license='LICENSE.txt',
+    long_description=open(__readme__).read(),
+    packages=['plantpredict'],
+    python_requires='>=3.5, <4',
+    install_requires=[
+        'requests',
+        'pandas',
+        'six',
+        'mock',
+        'xlrd',
+        'openpyxl'
+    ]
+)
+>>>>>>> parent of 635ccf7 (Version 1.0.10)
